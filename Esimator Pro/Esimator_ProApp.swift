a6944099c25a4298578115295b6bc43aaeb50491
Curtis Bollinger <bollingerceb421@gmail.com>
2025-11-18 18:15:06 -0500

    Initial Commit

---
+++ Esimator Pro/Esimator_ProApp.swift
@@ -0,0 +1,17 @@
+//
+//  Esimator_ProApp.swift
+//  Esimator Pro
+//
+//  Created by Curtis Bollinger on 11/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Esimator_ProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
