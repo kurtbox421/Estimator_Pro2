e29d989d5584682cf9e526bb5b7f245b8b7c24fe
kurtbox421 <57237517+kurtbox421@users.noreply.github.com>
2025-12-03 20:12:08 -0500

    Add product URL support to Material model

--- ViewModels/Material.swift
+++ ViewModels/Material.swift
@@ -7,31 +7,40 @@
 
 import Foundation
 
-extension Material {
-    var total: Double {
-        return quantity * unitCost
-    }
-}
 struct Material: Identifiable, Codable {
     let id: UUID
     var name: String
     var quantity: Double
     var unitCost: Double
+    var productURL: URL?
+
+    var total: Double {
+        quantity * unitCost
+    }
 
     var cost: Double {
-        quantity * unitCost
+        total
     }
 
     init(
         id: UUID = UUID(),
         name: String,
         quantity: Double,
-        unitCost: Double
+        unitCost: Double,
+        productURL: URL? = nil
     ) {
         self.id = id
         self.name = name
         self.quantity = quantity
         self.unitCost = unitCost
+        self.productURL = productURL
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case quantity
+        case unitCost
+        case productURL
     }
 }
-
