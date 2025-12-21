6bf05d6bd1c8673df246dd116778b0717eb412de
kurtbox421 <57237517+kurtbox421@users.noreply.github.com>
2025-12-20 19:22:15 -0500

    Fix lossy decoding helpers

--- Models/DecodingHelpers.swift
+++ Models/DecodingHelpers.swift
@@ -2,18 +2,6 @@ import Foundation
 import FirebaseFirestore
 
 extension KeyedDecodingContainer {
-    private static var iso8601Formatter: ISO8601DateFormatter = {
-        let formatter = ISO8601DateFormatter()
-        formatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
-        return formatter
-    }()
-
-    private static var iso8601FormatterWithoutFractional: ISO8601DateFormatter = {
-        let formatter = ISO8601DateFormatter()
-        formatter.formatOptions = [.withInternetDateTime]
-        return formatter
-    }()
-
     func decodeLossyDoubleIfPresent(forKey key: Key) throws -> Double? {
         if let doubleValue = try decodeIfPresent(Double.self, forKey: key) {
             return doubleValue
@@ -30,28 +18,26 @@ extension KeyedDecodingContainer {
         return nil
     }
 
-    func decodeLossyDouble(forKey key: Key, default defaultValue: Double = 0) throws -> Double {
-        try decodeLossyDoubleIfPresent(forKey: key) ?? defaultValue
-    }
-
     func decodeLossyDateIfPresent(forKey key: Key) throws -> Date? {
-        if let dateValue = try? decodeIfPresent(Date.self, forKey: key), let dateValue {
+        if let dateValue = try decodeIfPresent(Date.self, forKey: key) {
             return dateValue
         }
-        if let timestampValue = try? decodeIfPresent(Timestamp.self, forKey: key), let timestampValue {
+        if let timestampValue = try decodeIfPresent(Timestamp.self, forKey: key) {
             return timestampValue.dateValue()
         }
-        if let doubleValue = try? decodeLossyDoubleIfPresent(forKey: key), let doubleValue {
+        if let doubleValue = try decodeLossyDoubleIfPresent(forKey: key) {
             return Date(timeIntervalSince1970: doubleValue)
         }
-        if let stringValue = try? decodeIfPresent(String.self, forKey: key), let stringValue {
+        if let stringValue = try decodeIfPresent(String.self, forKey: key) {
             let trimmed = stringValue.trimmingCharacters(in: .whitespacesAndNewlines)
-            if let dateValue = Self.iso8601Formatter.date(from: trimmed) {
+            let formatterWithFractional = ISO8601DateFormatter()
+            formatterWithFractional.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
+            if let dateValue = formatterWithFractional.date(from: trimmed) {
                 return dateValue
             }
-            if let dateValue = Self.iso8601FormatterWithoutFractional.date(from: trimmed) {
-                return dateValue
-            }
+            let formatterWithoutFractional = ISO8601DateFormatter()
+            formatterWithoutFractional.formatOptions = [.withInternetDateTime]
+            return formatterWithoutFractional.date(from: trimmed)
         }
         return nil
     }
